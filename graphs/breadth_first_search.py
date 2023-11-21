ff0f8404696b8fa2445b7831c693b9f8d1fafa92
Marcin Augustynów <105516855+Alcares@users.noreply.github.com>
2023-11-21 15:56:10 +0200

    Create breadth_first_search.py

---
+++ graphs/breadth_first_search.py
@@ -0,0 +1,28 @@
+the_most_dangerous_graph = {
+    'lava': set(['sharks', 'piranhas']),
+    'sharks': set(['lava', 'bees', 'lasers']),
+    'piranhas': set(['lava', 'crocodiles']),
+    'bees': set(['sharks']),
+    'lasers': set(['sharks', 'crocodiles']),
+    'crocodiles': set(['piranhas', 'lasers'])
+  }
+
+def bfs(graph, start_vertex, target_value):
+  path = [start_vertex]
+  bfs_queue = [[start_vertex, path]]
+  visited = set()
+  
+  while bfs_queue:
+    current_vertex, path = bfs_queue.pop(0)
+    visited.add(current_vertex)
+    
+    for neighbor in graph[current_vertex]:
+      # Finish the function here:
+      if neighbor not in visited:
+        if neighbor is target_value:
+          return path + [neighbor]
+        else:
+          bfs_queue.append([neighbor, path + [neighbor]])
+      
+# Call bfs() below and print the result:
+print(bfs(the_most_dangerous_graph, "crocodiles", "bees"))
